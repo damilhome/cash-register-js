3eb81776e2ef90c24e4f2a619aa9bf4e7d771452
Danilo Almeida Milhome <damilhome@gmail.com>
2024-12-04 17:32:24 -0300

    verifica troco do cliente

--- script.js
+++ script.js
@@ -38,6 +38,60 @@ const loadDrawerChanges = () => {
     })
 }
 
+const checkAmount = (customerChange, amountToCheck, currencyIndex) => {
+    // TODO: corrigir lógica do cid[currencyIndex][1] >= amountToCheck
+    // amount não precisa ser menor, basta eu conseguir tirar a parte do cid dele
+    while(customerChange >= amountToCheck && cid[currencyIndex][1] >= amountToCheck) {
+        customerChange -= amountToCheck;
+        cid[currencyIndex][1] -= amountToCheck;
+        currencyIndex[currencyIndex][1] += 1;
+    }
+    return customerChange;
+}
+
+purchaseBtn.addEventListener('click', () => {
+    const customerCash = cash.value;
+
+    if(!customerCash){
+        return;
+    }
+
+    if(customerCash < price) {
+        alert('Customer does not have enough money to purchase the item');
+    } else if(customerCash === price) {
+        changeDue.innerHTML = `<p>No change due - customer paid with exact cash</p>`
+    } else {
+        // TODO: tem um jeito melhor de fazer essas chamadas?
+        let customerChange = checkAmount(customerCash - price, 100, 8);
+        customerChange = checkAmount(customerChange, 20, 7);
+        customerChange = checkAmount(customerChange, 10, 6);
+        customerChange = checkAmount(customerChange, 5, 5);
+        customerChange = checkAmount(customerChange, 1, 4);
+        customerChange = checkAmount(customerChange, 0.25, 3);
+        customerChange = checkAmount(customerChange, 0.1, 2);
+        customerChange = checkAmount(customerChange, 0.05, 1);
+        customerChange = checkAmount(customerChange, 0.01, 0);
+        
+        if(customerChange === 0) {
+            isCashInDrawerEqualToChange = true;
+            cid.forEach(currency => {
+                if(currency[1] > 0) {
+                    isCashInDrawerEqualToChange = false
+                }
+            })
+
+            if(isCashInDrawerEqualToChange) {
+                // TODO: implementar código para inserir o troco do cliente na tela com STATUS: CLOSED
+            } else {
+                // TODO: implementar código para inserir o troco do cliente na tela com STATUS: OPEN
+            }
+            
+        } else {
+            changeDue.innerHTML = `<p>Status: INSUFFICIENT_FUNDS</p>`
+        }
+    }
+})
+
 document.addEventListener('DOMContentLoaded', () => {
     loadPrice();
     loadDrawerChanges();
