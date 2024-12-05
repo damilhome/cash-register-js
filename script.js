f8b814c9df9dd8118caa5ad5ddc99da51e4da128
Danilo Almeida Milhome <damilhome@gmail.com>
2024-12-05 12:42:02 -0300

    melhoria de código

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const changeDue = document.getElementById('change-due');
-const cash = document.getElementById('cash');
+const changeDueElement = document.getElementById('change-due');
+const cashElement = document.getElementById('cash');
 const purchaseBtn = document.getElementById('purchase-btn');
-const purchasePrice = document.getElementById('drawer-purchase-price');
-const changeInDrawer = document.querySelectorAll('.drawer-change span');
+const purchasePriceElement = document.getElementById('drawer-purchase-price');
+const changeInDrawerElements = document.querySelectorAll('.drawer-change span');
+
 const currencyChange = [
     ['PENNY', 0],
     ['NICKEL', 0],
@@ -15,8 +16,8 @@ const currencyChange = [
     ['ONE HUNDRED', 0]
 ]
 
-let price = 3.26;
-let cid = [
+const price = 3.26;
+const cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
   ['DIME', 3.1],
@@ -29,41 +30,46 @@ let cid = [
 ];
 
 const loadPrice = () => {
-    purchasePrice.textContent = `${price}`;
-}
+    purchasePriceElement.textContent = `${price}`;
+};
 
 const loadDrawerChanges = () => {
-    changeInDrawer.forEach((change, index) => {
+    changeInDrawerElements.forEach((change, index) => {
         change.textContent = cid[index][1];
     })
-}
+};
 
-const checkAmount = (customerChange, amountToCheck, currencyIndex) => {
+const loadChangeDueElement = (status) => {
+    changeDueElement.innerHTML = `<p>Status: ${status}</p>`
+
+    for(let i = currencyChange.length - 1; i >= 0; i--) {
+        if(currencyChange[i][1] > 0) {
+            changeDueElement.innerHTML += `<p> ${currencyChange[i][0]}: $${currencyChange[i][1]}</p>`
+        }
+    }
+};
+
+const initialize = () => {
+    loadPrice();
+    loadDrawerChanges();
+};
+
+const updateChange = (customerChange, amountToCheck, currencyIndex) => {
     while(customerChange >= amountToCheck && cid[currencyIndex][1] > 0) {
         customerChange = parseFloat((customerChange - amountToCheck).toFixed(2));
         cid[currencyIndex][1] = parseFloat((cid[currencyIndex][1] - amountToCheck).toFixed(2));
         currencyChange[currencyIndex][1] = parseFloat((currencyChange[currencyIndex][1] + amountToCheck).toFixed(2));
     }
     return customerChange;
-}
+};
 
-const updateChangeDue = (status) => {
-    changeDue.innerHTML = `<p>Status: ${status}</p>`
-
-    for(let i = currencyChange.length - 1; i >= 0; i--) {
-        if(currencyChange[i][1] > 0) {
-            changeDue.innerHTML += `<p> ${currencyChange[i][0]}: $${currencyChange[i][1]}</p>`
-        }
-    }
-}
-
-const resetChange = () => {
+const resetCurrencyChange = () => {
     currencyChange.forEach(currency => currency[1] = 0);
 };
 
 purchaseBtn.addEventListener('click', () => {
-    const customerCash = Number(cash.value);
-    cash.value = '';
+    const customerCash = Number(cashElement.value);
+    cashElement.value = '';
 
     if(!customerCash){
         return;
@@ -72,35 +78,38 @@ purchaseBtn.addEventListener('click', () => {
     if(customerCash < price) {
         alert('Customer does not have enough money to purchase the item');
     } else if(customerCash === price) {
-        changeDue.innerHTML = `<p>No change due - customer paid with exact cash</p>`
+        changeDueElement.innerHTML = `<p>No change due - customer paid with exact cash</p>`
     } else {
         let customerChange = parseFloat((customerCash - price).toFixed(2));
         const currencyValues = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
         for(let i = currencyValues.length - 1; i >= 0; i--) {
-            customerChange = checkAmount(customerChange, currencyValues[i], i)
+            customerChange = updateChange(customerChange, currencyValues[i], i)
         }
 
-        console.log(typeof customerChange)
         if(customerChange === 0) {
             let isCashInDrawerEqualToChange = true;
             cid.forEach(currency => isCashInDrawerEqualToChange = currency[1] > 0 ? false : true)
 
             if(isCashInDrawerEqualToChange) {
-                updateChangeDue('CLOSED');
+                loadChangeDueElement('CLOSED');
             } else {
-                updateChangeDue('OPEN');
+                loadChangeDueElement('OPEN');
             }
             
         } else {
-            changeDue.innerHTML = `<p>Status: INSUFFICIENT_FUNDS</p>`
+            changeDueElement.innerHTML = `<p>Status: INSUFFICIENT_FUNDS</p>`
         }
 
-        resetChange();
+        resetCurrencyChange();
         loadDrawerChanges();
+    }
+});
+
+cashElement.addEventListener('keydown', e => {
+    if(e.key === 'Enter') {
+        e.preventDefault();
+        purchaseBtn.click();
     }
 })
 
-document.addEventListener('DOMContentLoaded', () => {
-    loadPrice();
-    loadDrawerChanges();
-})
+document.addEventListener('DOMContentLoaded', initialize);
